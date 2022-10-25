fbe44d045c6640f8982fa41e391624827a503d41
ZionBlade <wernerct@mail.uc.edu>
2022-10-25 12:38:17 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String [] names = {"tom", "Dick", "Harry"}; //array literal
+        //when you make an array, everything has to be the same type
+        System.out.println(names.length);
+
+        System.out.println(names[0]); //tom
+    }
+}
